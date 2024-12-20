d9e1f5f0c2133631d0548639f631272b99c09764
Austin Kabanda <96333691+xen-py@users.noreply.github.com>
2024-12-19 22:49:48 -0700

    Update AccountInformation.py

--- AccountInformation.py
+++ AccountInformation.py
@@ -1,5 +1,5 @@
 
-my_accounts =  [["he_need_summ_milk", "[REDACTED]"], ["xen_py", "[REDACTED]"]]
+my_accounts =  [["he_need_summ_milk", "[REDACTED]"], ["xen.py", "[REDACTED]"]]
 
 g_user = gmail[0]
 g_pass = gmail[1]
